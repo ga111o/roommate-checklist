a057b0239c6e0d0719631b05b6bb3eb399ec4794
ga111o <ga111o@proton.me>
2024-06-17 14:31:34 +0900

    💄 background color

--- roommate-checklist/src/index.js
+++ roommate-checklist/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import Input from "./input";
 import reportWebVitals from "./reportWebVitals";
 
@@ -9,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
+
     <Input />
   </React.StrictMode>
 );
